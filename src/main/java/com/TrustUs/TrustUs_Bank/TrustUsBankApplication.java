d1c9acd35e6e0efaa3537d1040bdc19d094c30b0
thisisvaidansh <vaidanshgodbole@gmail.com>
2026-07-05 17:45:18 +0530

    connected to database

---
+++ src/main/java/com/TrustUs/TrustUs_Bank/TrustUsBankApplication.java
@@ -0,0 +1,13 @@
+package com.TrustUs.TrustUs_Bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrustUsBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrustUsBankApplication.class, args);
+	}
+
+}
